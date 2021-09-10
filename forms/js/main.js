bdb7c7c70d2b552d08a1351d1b64af9954be095d
Nathan-cloud <59859702+Nathan-cloud@users.noreply.github.com>
2021-09-10 16:28:44 +0200

    initial commit

--- forms/js/main.js
+++ forms/js/main.js
@@ -1,7 +1,75 @@
-(function($){"use strict";$('.input3').each(function(){$(this).on('blur',function(){if($(this).val().trim()!=""){$(this).addClass('has-val');}
-else{$(this).removeClass('has-val');}})})
-$("#radio1").on('change',function(){if($(this).is(":checked")){$('.input3-select').slideUp(300);}});$("#radio2").on('change',function(){if($(this).is(":checked")){$('.input3-select').slideDown(300);}});var name=$('.validate-input input[name="name"]');var email=$('.validate-input input[name="email"]');var message=$('.validate-input textarea[name="message"]');$('.validate-form').on('submit',function(){var check=true;if($(name).val().trim()==''){showValidate(name);check=false;}
-if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/)==null){showValidate(email);check=false;}
-if($(message).val().trim()==''){showValidate(message);check=false;}
-return check;});$('.validate-form .input3').each(function(){$(this).focus(function(){hideValidate(this);});});function showValidate(input){var thisAlert=$(input).parent();$(thisAlert).addClass('alert-validate');}
-function hideValidate(input){var thisAlert=$(input).parent();$(thisAlert).removeClass('alert-validate');}})(jQuery);
+(function($) {
+    $('#meal_preference').parent().append('<ul class="list-item" id="newmeal_preference" name="meal_preference"></ul>');
+    $('#meal_preference option').each(function() {
+        $('#newmeal_preference').append('<li value="' + $(this).val() + '">' + $(this).text() + '</li>');
+    });
+    $('#meal_preference').remove();
+    $('#newmeal_preference').attr('id', 'meal_preference');
+    $('#meal_preference li').first().addClass('init');
+    $("#meal_preference").on("click", ".init", function() {
+        $(this).closest("#meal_preference").children('li:not(.init)').toggle();
+    });
+    var allOptions = $("#meal_preference").children('li:not(.init)');
+    $("#meal_preference").on("click", "li:not(.init)", function() {
+        allOptions.removeClass('selected');
+        $(this).addClass('selected');
+        $("#meal_preference").children('.init').html($(this).html());
+        allOptions.toggle();
+    });
+    var marginSlider = document.getElementById('slider-margin');
+    if (marginSlider != undefined) {
+        noUiSlider.create(marginSlider, {
+            start: [500],
+            step: 10,
+            connect: [true, false],
+            tooltips: [true],
+            range: {
+                'min': 0,
+                'max': 1000
+            },
+            format: wNumb({
+                decimals: 0,
+                thousand: ',',
+                prefix: '$ ',
+            })
+        });
+    }
+    $('#reset').on('click', function() {
+        $('#register-form').reset();
+    });
+    $('#register-form').validate({
+        rules: {
+            first_name: {
+                required: true,
+            },
+            last_name: {
+                required: true,
+            },
+            company: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            phone_number: {
+                required: true,
+            }
+        },
+        onfocusout: function(element) {
+            $(element).valid();
+        },
+    });
+    jQuery.extend(jQuery.validator.messages, {
+        required: "",
+        remote: "",
+        email: "",
+        url: "",
+        date: "",
+        dateISO: "",
+        number: "",
+        digits: "",
+        creditcard: "",
+        equalTo: ""
+    });
+})(jQuery);
